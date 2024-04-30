c057861ea510c3e782ed395ab2ad0146c6f7d4d4
gpego <giovipegoraro@gmail.com>
2024-04-30 16:20:19 +0200

    Popup menu working

--- utils/scroll.js
+++ utils/scroll.js
@@ -3,6 +3,6 @@ window.addEventListener("scroll", function() {
     if (window.scrollY > 300) {
         navbar.style.top = "0"; // Show the menu
     } else {
-        navbar.style.top = "-50px"; // Hide the menu
+        navbar.style.top = "-60px"; // Hide the menu
     }
 });

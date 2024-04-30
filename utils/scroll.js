3f63b448f5a65cdf54df7105b8a4a0ec40a6e5bb
gpego <giovipegoraro@gmail.com>
2024-04-30 22:08:30 +0200

    Added contacts

--- utils/scroll.js
+++ utils/scroll.js
@@ -1,6 +1,6 @@
 window.addEventListener("scroll", function() {
     const navbar = document.querySelector(".topnav");
-    if (window.scrollY > 300) {
+    if (window.scrollY > 250) {
         navbar.style.top = "0"; // Show the menu
     } else {
         navbar.style.top = "-60px"; // Hide the menu
